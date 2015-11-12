4a39c61670e554ad0946260107f799e795522e70
Toni Anne Romero <tonianne.romero@gmail.com>
2015-11-12 22:37:29 +0800

    basic functions to add sushi and comment. apear list on page

--- controllers/sushi.js
+++ controllers/sushi.js
@@ -1,12 +1,40 @@
-var express    = require('express'),
-    router     = express.Router(),
-    bodyParser = require('body-parser'),
-    methodOverride = require('method-override');
+var express         = require('express'),
+    router          = express.Router(),
+    bodyParser      = require('body-parser'),
+    methodOverride  = require('method-override');
 
-var Sushi = require('../models/sushi').model;
+var Sushi           = require('../models/sushi'),
+    User            = require('../models/user'),
+    IngredientModel = require('../models/ingredients').model;
 
 router.get("/sushi", function(req, res){
-  res.send('hello');
+    Sushi.find({}, function (err, sushis) {
+      res.render('sushi', { sushis: sushis });
+    });
+  })
+
+router.post("/sushi", function(req, res){
+  Sushi.create(req.body.sushi, function (err, sushi) {
+    if (err){
+      res.send("something wrong happened"+ err)
+    } else {
+      res.redirect('/sushi');
+    }
+  });
 })
 
+router.get("/sushi/:id/favourite", function(req, res){
+  Sushi.findByIdAndUpdate(req.params.id, {status: "favourite"}, function(err, sushi){
+    res.redirect('/sushi');
+  })
+});
+
+router.get("/sushi/:id/SUPER", function(req, res){
+  Sushi.findByIdAndUpdate(req.params.id, {status: "SUPER favourite"}, function(err, sushi){
+    res.redirect('/sushi');
+  })
+});
+
 module.exports = router;
+
+
--- models/sushi.js
+++ models/sushi.js
@@ -1,13 +1,14 @@
 var mongoose = require('mongoose');
-var ingredientSchema = require('./ingredients').schema
+// var ingredientSchema = require('./ingredients').schema
 
 // create a schema
 var sushiSchema = new mongoose.Schema({
   sushi_name:   String,
+  comment:      String,
   // reference to user_id
-  user:         [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+  // user:         [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
   // reference to embeded_schema
-  ingredients:  [ingredientSchema],
+  // ingredients:  [ingredientSchema],
   status:       { type: String, enum: ["favourite", "SUPER favourite"] },
 
   created_at:   Date,
